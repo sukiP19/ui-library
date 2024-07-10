9bd3081f7c3a23fbbb497df782a1886305a85da1
Suki Phan <suki@kiskolabs.com>
2024-07-10 15:09:56 +0300

    config vite

--- ui-library/vite.config.ts
+++ ui-library/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime'
+        }
+      }
+    }
+  }
 })
